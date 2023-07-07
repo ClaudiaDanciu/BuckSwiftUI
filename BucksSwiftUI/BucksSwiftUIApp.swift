64abd97c60c80d47f861a9ced7b5831074d52caa
CDN11 <contact@claudiadanciu.com>
2023-07-07 11:02:30 +0100

    Initial Commit

---
+++ BucksSwiftUI/BucksSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucksSwiftUIApp.swift
+//  BucksSwiftUI
+//
+//  Created by Claudia Danciu on 07/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct BucksSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
