019cc43fdf1ae5536b5777421380d526efbeb3a2
CDN11 <contact@claudiadanciu.com>
2023-07-12 17:15:54 +0100

    Add to home page user and category

--- BucksSwiftUI/Core/Home/HomeView/HomeView.swift
+++ BucksSwiftUI/Core/Home/HomeView/HomeView.swift
@@ -7,9 +7,54 @@
 
 import SwiftUI
 
+struct Category: Identifiable {
+    let id = UUID()
+    let person: String
+    var tasks: [ToDoItem]
+}
+
+
 struct HomeView: View {
+    @State private var categories: [Category] = []
+    
     var body: some View {
-        Text("Hello, Home!")
+        NavigationView {
+            List {
+                ForEach(categories) { category in
+                    Section(header: Text(category.person)) {
+                        ForEach(category.tasks) { item in
+                            Text(item.title)
+                        }
+                    }
+                }
+            }
+            .listStyle(PlainListStyle())
+            .navigationBarTitle("Home")
+        }
+        .navigationViewStyle(StackNavigationViewStyle())
+        .edgesIgnoringSafeArea(.bottom) // Extend the content to the bottom edge
+        .onAppear {
+            fetchTasks()
+        }
+    }
+    
+    private func fetchTasks() {
+        FirestoreManager.shared.fetchTasks { [self] (tasks, error) in
+            if let error = error {
+                print("Failed to fetch tasks: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let tasks = tasks else { return }
+            
+            // Group tasks by person
+            let groupedTasks = Dictionary(grouping: tasks, by: { $0.person })
+            
+            // Create categories with tasks for each person
+            categories = groupedTasks.map { person, tasks in
+                Category(person: person, tasks: tasks)
+            }
+        }
     }
 }
 
--- BucksSwiftUI/Core/Root/RootView.swift
+++ BucksSwiftUI/Core/Root/RootView.swift
@@ -7,14 +7,13 @@
 
 import SwiftUI
 
-
 struct RootView: View {
     @State var selectedTab: Tabs = .homeTab
 
     @EnvironmentObject var viewModel: AuthViewModel
     
     var body: some View {
-        Group {
+        VStack(spacing: 0) { // Use a VStack and set spacing to 0
             if viewModel.userSession != nil {
                 if selectedTab == .profileTab {
                     ProfileView()
@@ -22,19 +21,16 @@ struct RootView: View {
                 else if selectedTab == .homeTab{
                     HomeView()
                 }
-                VStack {
-                    Spacer()
-                    TabBarView(selectedTab: $selectedTab)
-                }
             } else {
                 LoginView()
             }
             
+            TabBarView(selectedTab: $selectedTab)
+                .padding(.bottom, 12) // Add bottom padding to the TabBarView
         }
+        .edgesIgnoringSafeArea(.bottom) // Extend the content to the bottom edge
     }
 }
-
-
 
 struct RootView_Previews: PreviewProvider {
     static var previews: some View {
