3ebfbaf9cc809bf8dca107b7493cb90710021208
CDN11 <contact@claudiadanciu.com>
2023-07-13 00:24:11 +0100

    Enable Swift UI tests and add a small test for the bar

--- BucksSwiftUITests/BucksSwiftUITests.swift
+++ BucksSwiftUITests/BucksSwiftUITests.swift
@@ -6,6 +6,7 @@
 //
 
 import XCTest
+import SwiftUI
 @testable import BucksSwiftUI
 
 final class BucksSwiftUITests: XCTestCase {
@@ -18,19 +19,20 @@ final class BucksSwiftUITests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
+    func testTabNavigation() throws {
+        // Given
+        var selectedTab: Tabs = .homeTab
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        let tabBarView = TabBarView(selectedTab: Binding(
+            get: { selectedTab },
+            set: { selectedTab = $0 }
+        ))
+
+        // When
+        selectedTab = .profileTab
+
+        // Then
+        XCTAssertEqual(selectedTab, .profileTab, "Selected tab should be set to profileTab")
     }
 
 }
